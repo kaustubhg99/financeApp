241c29b62b46e13843559db4ee846d283e8a911f
KAUSTUBH <kaustubhg99@gmail.com>
2021-09-15 00:32:49 +0530

    add styling and new transaction form

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  transactionModalActive = {
+    'display':'none'
+  };
+  flag = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  openNewTransaction(toggle:String){
+    console.log("called");
+    
+    if (toggle =="open") {
+      this.transactionModalActive.display= "flex";
+    } else if(toggle =="close"){
+      this.transactionModalActive.display= "none";
+    }
+  }
 }
